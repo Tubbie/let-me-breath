e419819e964ea78b8460f7073e2808c4dfaec797
Tubbie <Tubbie@github.com>
2020-06-30 00:20:37 +0200

    Fixes

--- camera/record.py
+++ camera/record.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import time
 from datetime import datetime
@@ -5,9 +6,10 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
 print(current_time)
-os.system('echo "filename=' + current_time + 'tt.ts" > ~/picam/hooks/start_record')
-
+os.system('echo "filename=' + current_time + '.ts" > ~/picam/hooks/start_record')
 # Sleep in seconds.
 time.sleep(15)
-
 os.system("touch ~/picam/hooks/stop_record")
+
+os.system("./../tmp/first.sh sdfdsf &")
+print("Done")
--- upload/dropbox_upload.py
+++ upload/dropbox_upload.py
@@ -6,16 +6,17 @@ import requests
 url = 'https://content.dropboxapi.com/2/files/upload'
 access_token = 'access-token'  # Add access token here.
 
-for fname in os.listdir('/home/pi/picam/archive'):  # /home/pi/picam/archive
+for fname in os.listdir('.'):  # /home/pi/picam/archive
     if fname.endswith('.txt'):
         print("Found" + fname)
         try:
-            x = requests.post(url, data="TestUpload", headers={
-                "Authorization": "Bearer " + access_token,
-                "Dropbox-API-Arg": "{\"path\": \"/" + datetime.date.today().strftime(
-                    "%Y-%m-%d") + "/" + fname + "\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}",
-                "Content-Type": "application/octet-stream"
-            })
+            with open(fname, "r") as file:
+                x = requests.post(url, data=file, headers={
+                    "Authorization": "Bearer " + access_token,
+                    "Dropbox-API-Arg": "{\"path\": \"/" + datetime.date.today().strftime(
+                        "%Y-%m-%d") + "/" + fname + "\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}",
+                    "Content-Type": "application/octet-stream"
+                })
         except requests.exceptions.RequestException as e:
             print("Error in upload")
         print("Uploaded")
