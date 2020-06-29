941a8e914e10683aa283e8e199603f860ca335a6
Tubbie <Tubbie@github.com>
2020-06-30 00:27:04 +0200

    Test record time.

--- camera/record.py
+++ camera/record.py
@@ -8,7 +8,7 @@ current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
 print(current_time)
 os.system('echo "filename=' + current_time + '.ts" > ~/picam/hooks/start_record')
 # Sleep in seconds.
-time.sleep(15)
+time.sleep(59) #Test
 os.system("touch ~/picam/hooks/stop_record")
 
 os.system("./../upload/dropbox_upload.sh " + current_time + ".ts &")
