3f46abfa192ad1964a37d8d55a377090410c3be0
Tubbie <Tubbie@github.com>
2020-06-29 16:08:04 +0200

    Fixes try and catch.

--- upload/dropbox_upload.py
+++ upload/dropbox_upload.py
@@ -4,15 +4,18 @@ import os
 import requests
 
 url = 'https://content.dropboxapi.com/2/files/upload'
-access_token = 'access-token'
+access_token = 'access-token'  # Add access token here.
 
-for fname in os.listdir('.'):  # /home/pi/picam/archive
+for fname in os.listdir('/home/pi/picam/archive'):  # /home/pi/picam/archive
     if fname.endswith('.txt'):
         print("Found" + fname)
-        x = requests.post(url, data="TestUpload", headers={
-            "Authorization": "Bearer " + access_token,
-            "Dropbox-API-Arg": "{\"path\": \"/" + datetime.date.today().strftime(
-                "%Y-%m-%d") + "/" + fname + "\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}",
-            "Content-Type": "application/octet-stream"
-        })
+        try:
+            x = requests.post(url, data="TestUpload", headers={
+                "Authorization": "Bearer " + access_token,
+                "Dropbox-API-Arg": "{\"path\": \"/" + datetime.date.today().strftime(
+                    "%Y-%m-%d") + "/" + fname + "\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}",
+                "Content-Type": "application/octet-stream"
+            })
+        except requests.exceptions.RequestException as e:
+            print("Error in upload")
         print("Uploaded")
